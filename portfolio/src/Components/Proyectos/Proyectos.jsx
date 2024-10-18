f7e4fa0cdbf1a7eca8727ce3b7b033cf51b637fd
guadafestinese <guadalupefestinese@gmail.com>
2024-10-18 16:42:49 -0300

    .

--- portfolio/src/Components/Proyectos/Proyectos.jsx
+++ portfolio/src/Components/Proyectos/Proyectos.jsx
@@ -12,6 +12,9 @@ import chupitraki2 from '../../../public/chupitraki2.png';
 import chupitraki3 from '../../../public/chupitraki3.png';
 import chupitraki4 from '../../../public/chupitraki4.png';
 import styled from "styled-components";
+import imgLink from '../../../public/enlace.png'
+import imgLink2 from '../../../public/enlace2.png'
+import { Link } from "react-router-dom";
 
 const CarouselContainer = styled.div`
 display:flex;
@@ -23,15 +26,53 @@ flex-wrap:wrap;
 export default function Proyectos() {
 
     const imagesWidi = [widi1, widi2, widi3, widi4, widi5, widi6, widi7, widi8];
-    const imagesChupitraki = [chupitraki1,chupitraki2,chupitraki3,chupitraki4];
+    const imagesChupitraki = [chupitraki1, chupitraki2, chupitraki3, chupitraki4];
     return (
         <div>
             <h1>Proyectos</h1>
             <CarouselContainer>
-            <Carousel images={imagesWidi} />
-            <Carousel images={imagesChupitraki} />
+                <div>
+                    <div className="flex justify-center items-center">
+                        <Link to='https://github.com/guadafestinese96/widistore' target="_blank">
+                            <div className="flex m-1 justify-center items-center bg-[#ED6BBF] w-20 rounded">
+                                <p className='m-1'>Code</p>
+                                <img src={imgLink} className="w-6 h-6" alt='imgLink' />
+                            </div>
+                        </Link>
+                        <Link to='https://widistore.vercel.app/' target="_blank">
+                            <div className="flex m-1 justify-center items-center bg-[#ED6BBF] w-20 rounded">
+                                <p className='m-1'>Page</p>
+                                <img src={imgLink} className="w-6 h-6" alt='imgLink' />
+                            </div>
+                        </Link>
+                    </div>
+                    <Carousel images={imagesWidi} />
+                </div>
+                <div>
+                    <div className="flex justify-center items-center">
+                        <Link to='https://github.com/guadafestinese96/chupitraki-page' target="_blank">
+                            <div className="flex m-1 justify-center items-center bg-[#ED6BBF] w-20 rounded">
+                                <p className='m-1'>Code</p>
+                                <img src={imgLink} className="w-6 h-6" alt='imgLink' />
+                            </div>
+                        </Link>
+                        <Link to='https://distribuidora-chupitraki.vercel.app/' target="_blank">
+                            <div className="flex m-1 justify-center items-center bg-[#ED6BBF] w-20 rounded">
+                                <p className='m-1'>Page</p>
+                                <img src={imgLink} className="w-6 h-6" alt='imgLink' />
+                            </div>
+                        </Link>
+                    </div>
+                    <Carousel images={imagesChupitraki} />
+                </div>
+
+                
             </CarouselContainer>
         </div>
 
     )
 }
+
+/*
+https://widistore.vercel.app/<Carousel images={imagesChupitraki} />
+*/
